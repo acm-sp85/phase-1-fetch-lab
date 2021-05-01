7e7f050cb3c7402f53ae782368321e87dd1ce9cd
acm-sp85 <acontell@gmail.com>
2021-05-01 12:45:13 -0400

    completed assignment

--- index.js
+++ index.js
@@ -1,29 +1,33 @@
-function fetchBooks() {
+document.addEventListener('DOMContentLoaded', function() {
+  fetchBooks();
+});
 
+
+function fetchBooks(){
   // To pass the tests, don't forget to return your fetch!
-  fetch('https://anapioficeandfire.com/api/books')
+     
+  return (fetch('https://anapioficeandfire.com/api/books')
   .then(response => response.json())
-  .then(data => renderBooks(data));
-
-
-}
-
+  .then(json => {
+    return renderBooks(json)
+  })
+)
   
-function renderBooks(books) {
-  console.log('this is beign executed');
-  console.log(`${books}`)
+}
+  
 
+
+function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
     const h2 = document.createElement('h2');
-    h2.innerText = book.name;
+    h2.innerHTML = book.name;
     main.appendChild(h2);
+
   });
+
+
 }
 
-document.addEventListener('DOMContentLoaded', function() {
-  fetchBooks();
+
   
-
-
-});
